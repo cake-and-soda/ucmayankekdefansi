e089e9274fe3b8869e742568f364deb4ec65c805
Ömer Çetiner <omer39ctnr@gmail.com>
2024-01-05 17:31:15 +0300

    new day new year

---
+++ CakeAndSoda/CakeAndSoda/Models/Urun_Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CakeAndSoda.Models
+{
+    public class Urun_Model
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public int Kod { get; set; }
+        public string Detay { get; set; }
+    }
+}
