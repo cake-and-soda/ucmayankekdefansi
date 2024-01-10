416f89d713b70233761c1a8ab55e20845ca149a0
eefee <124432670+676exe@users.noreply.github.com>
2024-01-10 15:46:13 +0300

    Admin Panel

---
+++ Admin Panel MVC/WebApplication1/WebApplication1/Controllers/UrunController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        public ActionResult Ekle()
+        {
+            //sa burası urun ekleme sayfası niggalar
+            return View();
+        }
+        public ActionResult Ekle (Urun urun)
+        {
+            //burada ürün de kaydediyoz veri tabanınada kaydediyoz kaliteli yeri niggalar
+            // _context.Urunler.Add(urun);
+            // _context.SaveChanges();
+            return RedirectToAction("Liste");
+        }
+
+        public ActionResult Duzenle (int id)
+        {
+            //burası ürün düzenleme sayfası niggalar
+            return View();
+        }
+        public ActionResult Duzenle (Urun urun)
+        {
+            //güncelleme yapıyoruz ayrıca veri tabanı güncelleme burada niggalar 
+            return RedirectToAction("Liste");
+        }
+        //daha da yazmıyorum zaten biliyonuz
+
+        public ActionResult Sil (int id)
+        {
+            return RedirectToAction("Liste");
+        } 
+        public ActionResult Liste ()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Admin Panel MVC/WebApplication1/WebApplication1/Models/Urun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Urun
+    {
+        public int urunId { get; set; }
+        public string Ad { get; set; }
+        public string Kod { get; set; }
+        public string Detay { get; set; }
+
+        public List<UrunRenk> Renkler { get; set; }
+    }
+
+    public class UrunRenk
+    {
+        public int RenkId { get; set; }
+        public int UrunId { get; set; }
+        public string Renk { get; set; }
+
+        public List<UrunBeden> Bedenler { get; set; }
+    }
+    
+    public class UrunBeden
+    {
+        public int BedenId { get; set; }
+        public int RenkId { get; set; }
+        public string BedenBoyutu { get; set; }
+        public string Stok { get; set; }
+    }
+
+
+    public class UrunResim
+    {
+        public int ResimId { get; set; }
+        public int RenkId { get; set; }
+        public string  ResimYolu { get; set; }
+    }
+}
