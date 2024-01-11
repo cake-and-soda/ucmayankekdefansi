4a16fe523b1b34181c4cce64e73e7f6c7ce15d6f
Ömer Çetiner <134373569+PenguBey@users.noreply.github.com>
2024-01-11 15:01:18 +0300

    ekle örnekledirildi

--- WorkSpace/WorkSpace/Controllers/YonetimController.cs
+++ WorkSpace/WorkSpace/Controllers/YonetimController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WorkSpace.Models;
 
 namespace WorkSpace.Controllers
 {
     public class YonetimController : Controller
     {
+        VtContex vt_islemleri = new VtContex();
         // GET: Yonetim
         public ActionResult Liste()
         {
@@ -16,6 +18,12 @@ namespace WorkSpace.Controllers
         public ActionResult Ekle()
         {
             return View();
+        }
+        public ActionResult Ekle1(Urun_Model gelen)
+        {
+            vt_islemleri.Urun.Add(gelen);
+            vt_islemleri.SaveChanges();
+            return View("Ekle");
         }
         public ActionResult Guncelle()
         {
